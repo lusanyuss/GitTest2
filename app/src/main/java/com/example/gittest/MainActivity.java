0345dce55f25f0a6a1fff6c3949d311a689d555e
yuliu <312832473@qq.com>
2020-04-17 00:05:36 +0800

    first

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.gittest;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public TextView mTextView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
